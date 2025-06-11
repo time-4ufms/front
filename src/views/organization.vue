2ee792b440c84f4834c9ae5bd6d3f26dc4e95ae0
FreddyGuedes <freddy.okino@trackland.com.br>
2025-06-11 13:59:43 -0400

    Descrição da organização

--- src/views/organization.vue
+++ src/views/organization.vue
@@ -43,5 +43,23 @@
             <img src="/public/img/xp.png" alt=""
                 style="height: 100%; text-align: center;border-radius: 25px;">
         </div>
+
+        <p style="margin-top: 20px;margin-bottom: 20px; font-size: 20px;">A XP (Extreme Programming) é uma metodologia
+            ágil que enfatiza a colaboração, feedback rápido e entrega contínua de software. Ela envolve práticas como
+            desenvolvimento orientado a testes (TDD), programação em par e integração contínua.</p>
+        <p style="margin-top: 20px;margin-bottom: 20px; font-size: 20px;">Durante o desenvolvimento desse projeto, o
+            time 4 utilizou as seguinte práticas da metodologia XP: Pair Programming e Integração contínua.</p>
+        <h3 style="margin-top: 20px;">Pair Programming</h3>
+        <p style="margin-top: 20px;margin-bottom: 20px; font-size: 20px;">O Pair Programming é uma prática onde dois
+            desenvolvedores trabalham juntos em um único computador, compartilhando o teclado e o mouse. Essa prática
+            promove a colaboração, revisão de código em tempo real e compartilhamento de conhecimento.</p>
+        <p style="margin-top: 20px;margin-bottom: 20px; font-size: 20px;">No projeto, o Pair Programming foi utilizado
+            para o desenvolvimento de funcionalidades específicas, permitindo que os membros da equipe colaborassem
+            diretamente e revisassem o código enquanto trabalhavam juntos.</p>
+        <h3 style="margin-top: 20px;">Integração Contínua</h3>
+        <p style="margin-top: 20px;margin-bottom: 20px; font-size: 20px;">A Integração Contínua é uma prática onde o
+            código é integrado e testado automaticamente em um repositório compartilhado. Isso permite que a equipe
+            identifique problemas rapidamente e mantenha a qualidade do código.</p>
+        <p style="margin-top: 20px;margin-bottom: 20px; font-size: 20px;">No projeto, a Integração Contínua foi em as alterações e incrementos nos códigos eram associados frequentemente nos repositórios centrais data, frontend, backend e docs, como pode ser visto através dos números de commits e pull-requests dos repositórios através desse link.</p>
     </div>
 </template>
